dc5ffa5b429b6606ba54f9b69d1ba5fafbede872
aasthacodehub <136153254+aasthacodehub@users.noreply.github.com>
2024-06-12 11:19:49 +0530

    added main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+var typed= new Typed(".text",{
+    strings: ["Frontend Developer","Web Developer","Researcher","Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+});
